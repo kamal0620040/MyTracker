ad6f7e6a9588f8931346cf2692dfcd343a9250e9
kamal0620040 <neupane0620040@outlook.com>
2022-03-23 09:45:42 +0545

    Setup Firebase for android. Other need to add google-service.json inside android/app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mytracker/provider/theme_provider.dart';
 import 'package:mytracker/utils/colors.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
