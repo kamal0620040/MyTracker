b6dc5991c3fdc027540de6206d0806698668e149
kamal0620040 <neupane0620040@outlook.com>
2022-03-23 17:54:05 +0545

    Made both login and signup page scrollable

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -28,144 +28,157 @@ class _LoginScreenState extends State<LoginScreen> {
           // padding: const EdgeInsets.only(all: 20.0),
           padding: const EdgeInsets.symmetric(horizontal: 20),
           width: double.infinity,
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            //Text field input for email
-            Flexible(
-              child: Container(),
-              flex: 1,
-            ),
-            Text(
-              "Welcome",
-              style: GoogleFonts.poppins(
-                textStyle:
-                    const TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-              ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            Text(
-              "Sign in to continue",
-              textAlign: TextAlign.right,
-              style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Color.fromRGBO(202, 202, 202, 1),
+          child: LayoutBuilder(builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 46,
-            ),
-            Text(
-              "Email",
-              style: GoogleFonts.poppins(
-                textStyle:
-                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFieldInput(
-              textEditingController: _emailController,
-              hintText: "Enter your email",
-              textInputType: TextInputType.text,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            //Text field input for password
-            Text(
-              "Password",
-              style: GoogleFonts.poppins(
-                textStyle:
-                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFieldInput(
-              textEditingController: _passwordController,
-              hintText: "Enter your password",
-              textInputType: TextInputType.text,
-              isPass: true,
-            ),
-            const SizedBox(
-              height: 28,
-            ),
-            //button Login
-            InkWell(
-              child: Container(
-                child: Text(
-                  "Log In",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                width: double.infinity,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                decoration: const ShapeDecoration(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
-                    ),
-                  ),
-                  color: Color.fromRGBO(101, 146, 233, 1),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            //transition for signin
-            Flexible(
-              child: Container(),
-              flex: 1,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text(
-                    "Don't have an account?",
-                    style: GoogleFonts.poppins(
-                      textStyle: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Color.fromRGBO(202, 202, 202, 1),
-                      ),
-                    ),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    child: Text(
-                      " Sign Up",
-                      style: GoogleFonts.poppins(
-                        textStyle: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromRGBO(101, 146, 233, 1),
+                child: IntrinsicHeight(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //Text field input for email
+                        Flexible(
+                          child: Container(),
+                          flex: 1,
                         ),
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                  ),
+                        Text(
+                          "Welcome",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        Text(
+                          "Sign in to continue",
+                          textAlign: TextAlign.right,
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromRGBO(202, 202, 202, 1),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 46,
+                        ),
+                        Text(
+                          "Email",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFieldInput(
+                          textEditingController: _emailController,
+                          hintText: "Enter your email",
+                          textInputType: TextInputType.text,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        //Text field input for password
+                        Text(
+                          "Password",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFieldInput(
+                          textEditingController: _passwordController,
+                          hintText: "Enter your password",
+                          textInputType: TextInputType.text,
+                          isPass: true,
+                        ),
+                        const SizedBox(
+                          height: 28,
+                        ),
+                        //button Login
+                        InkWell(
+                          child: Container(
+                            child: Text(
+                              "Log In",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                            width: double.infinity,
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            decoration: const ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(4),
+                                ),
+                              ),
+                              color: Color.fromRGBO(101, 146, 233, 1),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        //transition for signin
+                        Flexible(
+                          child: Container(),
+                          flex: 1,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: Text(
+                                "Don't have an account?",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color.fromRGBO(202, 202, 202, 1),
+                                  ),
+                                ),
+                              ),
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                child: Text(
+                                  " Sign Up",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color.fromRGBO(101, 146, 233, 1),
+                                    ),
+                                  ),
+                                ),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
+                              ),
+                            ),
+                          ],
+                        )
+                        // Text("Already have an account? Sign In",style: ,),
+                      ]),
                 ),
-              ],
-            )
-            // Text("Already have an account? Sign In",style: ,),
-          ]),
+              ),
+            );
+          }),
         ),
       ),
     );
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -25,167 +25,205 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
           // padding: const EdgeInsets.only(all: 20.0),
           padding: const EdgeInsets.symmetric(horizontal: 20),
           width: double.infinity,
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            //Text field input for email
-            Flexible(
-              child: Container(),
-              flex: 1,
-            ),
-            Text(
-              "Create an account",
-              style: GoogleFonts.poppins(
-                textStyle:
-                    const TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-              ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            Text(
-              "Sign up to continue",
-              textAlign: TextAlign.right,
-              style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Color.fromRGBO(202, 202, 202, 1),
+
+          child: LayoutBuilder(builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 46,
-            ),
-            Text(
-              "Name",
-              style: GoogleFonts.poppins(
-                textStyle:
-                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFieldInput(
-              textEditingController: _nameController,
-              hintText: "Enter your name",
-              textInputType: TextInputType.text,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Email",
-              style: GoogleFonts.poppins(
-                textStyle:
-                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFieldInput(
-              textEditingController: _emailController,
-              hintText: "Enter your email",
-              textInputType: TextInputType.text,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            //Text field input for password
-            Text(
-              "Password",
-              style: GoogleFonts.poppins(
-                textStyle:
-                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFieldInput(
-              textEditingController: _passwordController,
-              hintText: "Enter your password",
-              textInputType: TextInputType.text,
-              isPass: true,
-            ),
-            const SizedBox(
-              height: 28,
-            ),
-            //button signup
-            InkWell(
-              child: Container(
-                child: Text(
-                  "Create account",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                width: double.infinity,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                decoration: const ShapeDecoration(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
-                    ),
-                  ),
-                  color: Color.fromRGBO(101, 146, 233, 1),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            //transition for signin
-            Flexible(
-              child: Container(),
-              flex: 1,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text(
-                    "Already have an account?",
-                    style: GoogleFonts.poppins(
-                      textStyle: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Color.fromRGBO(202, 202, 202, 1),
-                      ),
-                    ),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    child: Text(
-                      " Sign In",
-                      style: GoogleFonts.poppins(
-                        textStyle: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromRGBO(101, 146, 233, 1),
+                child: IntrinsicHeight(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //Text field input for email
+                        Flexible(
+                          child: Container(),
+                          flex: 1,
                         ),
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                  ),
+                        Text(
+                          "Create an account",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        Text(
+                          "Sign up to continue",
+                          textAlign: TextAlign.right,
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromRGBO(202, 202, 202, 1),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        // circular widget to accept and show our selected file
+                        Stack(
+                          children: [
+                            const Center(
+                              child: CircleAvatar(
+                                radius: 64,
+                                backgroundImage: NetworkImage(
+                                    "https://scontent.fktm6-1.fna.fbcdn.net/v/t1.15752-9/275991517_379301487372923_3465182913503674080_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_ohc=yLeFaXWZzY4AX-nTDHQ&tn=xGAR1u5kKRoEgpGx&_nc_ht=scontent.fktm6-1.fna&oh=03_AVJFYXnpVK0LACxUDVgbI6-qN0kDGhF4fjspY8fVhKuBTw&oe=625F7450"),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: -10,
+                              left: 175,
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.add_a_photo),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          "Name",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFieldInput(
+                          textEditingController: _nameController,
+                          hintText: "Enter your name",
+                          textInputType: TextInputType.text,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Email",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFieldInput(
+                          textEditingController: _emailController,
+                          hintText: "Enter your email",
+                          textInputType: TextInputType.text,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        //Text field input for password
+                        Text(
+                          "Password",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFieldInput(
+                          textEditingController: _passwordController,
+                          hintText: "Enter your password",
+                          textInputType: TextInputType.text,
+                          isPass: true,
+                        ),
+                        const SizedBox(
+                          height: 28,
+                        ),
+                        //button signup
+                        InkWell(
+                          child: Container(
+                            child: Text(
+                              "Create account",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                            width: double.infinity,
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            decoration: const ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(4),
+                                ),
+                              ),
+                              color: Color.fromRGBO(101, 146, 233, 1),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        //transition for signin
+                        Flexible(
+                          child: Container(),
+                          flex: 1,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: Text(
+                                "Already have an account?",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color.fromRGBO(202, 202, 202, 1),
+                                  ),
+                                ),
+                              ),
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                child: Text(
+                                  " Sign In",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color.fromRGBO(101, 146, 233, 1),
+                                    ),
+                                  ),
+                                ),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
+                              ),
+                            ),
+                          ],
+                        )
+                        // Text("Already have an account? Sign In",style: ,),
+                      ]),
                 ),
-              ],
-            )
-            // Text("Already have an account? Sign In",style: ,),
-          ]),
+              ),
+            );
+          }),
         ),
       ),
     );
